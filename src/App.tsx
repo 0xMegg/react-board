c67e76651815b5d4917f0f489316ac9653a2bd6b
Megg <finero91@gmail.com>
2025-07-11 15:32:27 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import SigninPage from "@/pages/Signin";
+import SignupPage from "@/pages/Signup";
+import Home from "@/pages/Home";
+import { ThemeProvider } from "@/components/theme-provider";
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
